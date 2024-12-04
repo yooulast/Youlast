8c9cacbde08c71836d1da3785bf58db35d936e67
yooulast <youlast1333@gmail.com>
2024-12-05 02:17:16 +0300

    Update speed.py
    Signed-off-by: yooulast <youlast1333@gmail.com>

--- AnonXMusic/plugins/admins/speed.py
+++ AnonXMusic/plugins/admins/speed.py
@@ -14,7 +14,7 @@ checker = []
 
 
 @app.on_message(
-    filters.command(["cspeed", "speed", "cslow", "slow", "playback", "cplayback"])
+    filters.command(["cspeed", "سرعه", "cslow", "بطيء", "playback", "cplayback"])
     & filters.group
     & ~BANNED_USERS
 )
