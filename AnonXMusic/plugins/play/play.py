c3d1d007b5fe09cdb63baf824be8280f37eb72a6
yooulast <youlast1333@gmail.com>
2024-12-05 01:17:10 +0300

    Update play.py
    Signed-off-by: yooulast <youlast1333@gmail.com>

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -28,7 +28,7 @@ from config import BANNED_USERS, lyrical
 @app.on_message(
     filters.command(
         [
-            "play",
+            "شغل", "تشغيل"
             "vplay",
             "cplay",
             "cvplay",
