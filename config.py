46016e9d4a63bfd91f6cb06a5528b8b18e2793ce
yooulast <youlast1333@gmail.com>
2024-12-06 03:47:46 +0300

    Update config.py
    Signed-off-by: yooulast <youlast1333@gmail.com>

--- config.py
+++ config.py
@@ -83,16 +83,16 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
+STATS_IMG_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
+TELEGRAM_AUDIO_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
+TELEGRAM_VIDEO_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
+STREAM_IMG_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
+SOUNCLOUD_IMG_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
+YOUTUBE_IMG_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
+SPOTIFY_ARTIST_IMG_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
+SPOTIFY_ALBUM_IMG_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
+SPOTIFY_PLAYLIST_IMG_URL = "https://forkgraph.zaid.pro/file/tjc1YmSjvlJm"
 
 
 def time_to_seconds(time):
